3770152340510f4434137291da49875c9e78d5c7
Jeorge Donato <jeorgekhenr@gmail.com>
2020-11-16 16:49:22 -0800

    Modified server js to get the socket .io working

--- server.js
+++ server.js
@@ -1,25 +1,22 @@
 const express = require("express");
 const path = require("path");
-const PORT = process.env.PORT || 3002;
+const PORT = process.env.PORT || 8000;
 const connectDB = require("./config/db");
-const http = require("http");
-const socketIo = require("socket.io");
 const app = express();
+const http = require( "http" ).createServer( app );
+const io = require( "socket.io" )( http );
 const cors = require("cors");
 connectDB();
 
+//Declare Middlewares
 app.use(express.json({ extended: false }));
-
-const server = http.createServer(app);
-const io = socketIo(server);
-
-//Define routes here
+app.use(cors());
 app.use(function (req, res, next) {
   res.io = io;
   next();
 });
+//Define routes here
 app.use("/api/books", require("./controllers/book"));
-app.use(cors());
 
 // Serve static assets in production
 if (process.env.NODE_ENV === "production") {
@@ -31,15 +28,13 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-io.sockets.on("connection", function (socket) {
-  console.log("New client connected");
-  socket.on("disconnect", () => {
-    console.log("Client disconnected");
-  });
-  // socket.emit("to_react","lez do diz");
+io.on('connection',function(socket){  
+    console.log("A user is connected");
 });
 
-server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+http.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+
+
 
 // app.listen(PORT, () => {
 //   console.log(PORT)
